87b00ea8f068d5e2289378989b79a3d8b8d06e6c
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-09-01 10:46:23 -0400

    add list_of_countries() to utils

---
+++ R/utils.R
@@ -0,0 +1,33 @@
+add_and <- function(x) {
+  if (!(is.character(x))) {
+    warning("`x` must be character. coercing to character")
+    x <- as.character(x)
+  }
+
+  lx <- length(x)
+  if (lx == 1) {
+    y <- x
+  }
+  else if (lx == 2) {
+    y <- paste(x[1], "and", x[2])
+  }
+  else {
+    y <- c(x[1:lx-1], paste("and", x[lx]))
+    y <- paste(y, collapse = ", ")
+  }
+  return(y)
+}
+
+
+list_of_countries <- function(maindir = getOption("pip.maindir")) {
+
+  countries <- fs::dir_ls(path    = maindir,
+                          recurse = FALSE,
+                          type    = "directory"
+  )
+
+  country_list <- gsub(maindir, "", countries)
+  country_list <- country_list[!grepl("^_", country_list)]
+  return(country_list)
+}
+
