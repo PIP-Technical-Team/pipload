eb0ecfdcac75597fd1d964138c8f3e0e5632d781
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-12-08 12:26:45 -0500

    make sure append works properly in inventory

--- R/pip_update_inventory.R
+++ R/pip_update_inventory.R
@@ -240,7 +240,9 @@ pip_update_inventory <- function(country = NULL,
       setDT(df)
 
       df <- df[!(country_code %chin% diff_cty)]
-      dt <- rbindlist(list(dt, df), use.names = TRUE)
+      dt <- rbindlist(list(dt, df),
+                      use.names = TRUE,
+                      fill = TRUE)
     }
 
     # re-write inventory in production if data signature is not found
