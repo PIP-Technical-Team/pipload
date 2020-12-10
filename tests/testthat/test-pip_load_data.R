ad62cdeb0c6d83549b8d189ccf9b167603816d14
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-12-10 15:52:43 -0500

    update documentation

--- tests/testthat/test-pip_load_data.R
+++ tests/testthat/test-pip_load_data.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("return warning when surveys are not available", {
+  survey_id <-
+    c(
+      "PRY_2017_EPH_V01_M_V01_A_PIP_PC-GPWG",
+      "PRY_2018_EPH_V01_M_V02_A_PIP_PC-GPWG",
+      "x",
+      "y",
+      "z"
+    )
+  expect_warning(pip_load_data(survey_id = survey_id))
 })
+
+test_that("return NULL when only one wrong survey id is provided", {
+  expect_null(pip_load_data(survey_id = "x"))
+})
+
+
+
+
