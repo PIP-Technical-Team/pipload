745ae837b8a3d76f6ac8282e9fb853837eaf2658
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-12-22 11:25:32 -0500

    ADd filtering of data to PC to find_data

--- R/pip_find_data.R
+++ R/pip_find_data.R
@@ -19,6 +19,11 @@
 #' Can't be used with arguments `country`, `year`,
 #' `survey_acronym` , `vermast`, `veralt`, `module` or `tool`.
 #' @param maindir character: Main directory
+#' @param filter_to_pc logical: If TRUE filter most recent data to be included
+#' in the Poverty Calculator. Default if FALSE
+#' @param filter_to_tm logical: If TRUE filter most recent data to be included
+#' in the Table Maker. Default if FALSE
+#' @inheritParams pip_load_inventory
 #'
 #' @return data.frame: list of filenames to be loaded with pcn_load()
 #' @import data.table
@@ -72,9 +77,15 @@ pip_find_data <- function(country         = NULL,
                          tool             = NULL,
                          condition        = NULL,
                          source           = NULL,
-                         maindir          = getOption("pip.maindir")
+                         maindir          = getOption("pip.maindir"),
+                         inv_file         = paste0(maindir,
+                                           "_inventory/inventory.fst"),
+                         filter_to_pc = FALSE,
+                         filter_to_tm = FALSE
                          ) {
-  #--------- Initial conditions
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #---------   Initial conditions   ---------
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   # drive and main dir
 
@@ -90,14 +101,24 @@ pip_find_data <- function(country         = NULL,
                   )
   }
 
+  #--------- Filter conditions ---------
+  if (filter_to_pc == TRUE && filter_to_tm == TRUE) {
+    rlang::abort(c(
+      "Syntax error",
+      x = "`filter_to_pc` and `filter_to_tm` can't both be TRUE"
+    ),
+    class = "pipload_error"
+    )
+  }
+
 
   #----------------------------------------------------------
-  #   Country condition
+  #   Create conditions
   #----------------------------------------------------------
 
   if (!is.null(condition)) { # if condition is used
-
-    condi <- parse(text = condition)
+    condit <- condi
+    condi  <- parse(text = condition)
 
   } else { # if condition is not used
 
@@ -155,11 +176,6 @@ pip_find_data <- function(country         = NULL,
       }
     }   # end of country no NULL
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    #---------   Load Inventory database   ---------
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
     #--------- create conditions ---------
     # Country and year are special because the names are different
     condi <- "country_code %chin% (countries)"
@@ -190,13 +206,114 @@ pip_find_data <- function(country         = NULL,
     }
 
     #--------- load data ---------
-    condi <- parse(text = condi)
+    condit <- condi
+    condi  <- parse(text = condi)
 
   } # end of condition == NULL
 
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #---------   Load Inventory   ---------
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  df <- pip_load_inventory(maindir = maindir)
+
+  df <- pip_load_inventory(maindir  = maindir,
+                           inv_file = inv_file)
   df <- df[ eval(condi)]
+
+  if (nrow(df) == 0) {
+    cli::cli_alert_danger("The inventory has no data for this condition,
+                          {.field {condit}}")
+  }
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #--------- Filter Data to PC ingest   ---------
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  if (filter_to_pc == TRUE) {
+
+    df <-
+      df[ # keep just the ones used in PC
+        tool == "PC"
+      ][,
+        # Get max master version and filter
+        maxmast := vermast == max(vermast),
+        by = .(country_code, surveyid_year, survey_acronym, module)
+      ][
+        maxmast == 1
+      ][,
+        # Get max veralt version and filter
+        maxalt := veralt == max(veralt),
+        by = .(country_code, surveyid_year, survey_acronym, module)
+      ][,
+        c("maxalt",  "maxmast") := NULL
+      ][,
+        # Select right module (source) if more than one available
+        # Create grouping variable
+        survey_id := paste(country_code, surveyid_year, survey_acronym, vermast, veralt,
+                           sep = "_")
+      ]
+
+    du <- df[, # get unique source by ID
+             .(source = unique(source)),
+             by = survey_id
+    ][, # count sources by ID
+      n_source := .N,
+      by = survey_id
+    ]
+
+    #--------- Only if there are more than one source in at least one ID ---------
+
+    # those with only one source
+    du1 <-
+      du[n_source == 1
+      ][,
+        n_source := NULL
+      ]
+
+    # treatment for those with more than one source
+    du2 <-
+      du[n_source > 1
+      ][, # nest data by Survey ID. one dataframe for each ID with
+        # several sources.
+        .(data = .nest(source)),
+        by = survey_id
+
+      ][, # Keep one source per data using rule in `keep_source()`
+        filtered := purrr::map(data, ~sf_keep_pc_source(df = .x))
+      ]
+
+    if (inherits(du2$filtered, "list")) {
+
+      du2 <- du2[, # Unnest data again so we get one source per ID
+                 .(source = .unnest(filtered)),
+                 by = survey_id
+      ]
+    } else {
+      du2[,
+          source := filtered
+      ][,
+        c("data", "filtered") := NULL
+      ]
+    }
+
+    # Append both sources in one
+    dun <- data.table::rbindlist(list(du1, du2),
+                                 use.names = TRUE,
+                                 fill      = TRUE)
+
+    # Filter df with only the value in dun
+    df <- df[dun,
+             on = .(survey_id,source)]
+  }
+
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #--------- Filter Data to TM ingest   ---------
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  if (filter_to_pc == TRUE) {
+    df <- df
+  }
 
   return(df)
 }
@@ -204,8 +321,57 @@ pip_find_data <- function(country         = NULL,
 #---------   Auxiliary functions   ---------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#' CReate conditions for filtering
+#'
+#' @param x argument parsed through `pip_find_data`
+#'
+#' @return character
+#'
+#' @examples
 create_cond <- function(x) {
   cd <- paste0("& toupper(", x, ") %chin% (alt_", x, ")")
   return(cd)
 }
+
+
+#' Make sure we keep only one module per survey ID when loading.
+#'
+#' @param df dataframe from `pip_load_inventory()`
+#'
+#' @return
+#' @export
+#'
+#' @import data.table
+#'
+#' @examples
+pip_keep_pc_source <- function(df){
+
+  source_order <- c("GPWG", "HIST", "BIN", "GROUP", "synth")
+  source_avail <- df[, unique(source)]
+
+  out         <- FALSE
+  i           <- 0
+  maxi        <- length(source_order)
+  source_keep <- NULL
+  while(out == FALSE && i <= maxi) {
+
+    i <- i + 1
+    if (source_order[i] %in% source_avail) {
+      source_keep <- source_order[i]
+      out         <- TRUE
+    }
+
+  }
+
+  if (!is.null(source_keep)) {
+    df <- df[source == (source_keep)]
+  }
+  return(df)
+}
+
+
+
+# make sure function runs fine
+sf_keep_pc_source <- purrr::possibly(pip_keep_pc_source,
+                                     otherwise = NULL)
 
