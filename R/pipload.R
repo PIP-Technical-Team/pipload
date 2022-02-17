a1957a9f883331db3d203fd5c0950751b07202ad
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-02-17 15:05:17 -0500

    update documentation

---
+++ R/pipload.R
@@ -0,0 +1,23 @@
+#' A package for loading PIP data
+#'
+#'
+#' @section pipload functions:
+#' The pipaux functions ...
+#'
+#' @docType package
+#' @name pipload
+#' @import data.table
+#' @importFrom magrittr "%>%" "%<>%"
+#' @export
+magrittr::`%>%`
+
+# Make sure data.table knows we know we're using it
+.datatable.aware = TRUE
+
+# Prevent R CMD check from complaining about the use of pipe expressions
+# standard data.table variables
+if (getRversion() >= "2.15.1")
+  utils::globalVariables(c(".", ".I", ".N", ".SD"), utils::packageName())
+
+NULL
+
