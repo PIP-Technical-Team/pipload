6258388c52e118207457dfb0b9467bb3a71471ee
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-02-18 10:52:06 -0500

    change condition in creatino of globals

--- R/pip_create_globals.R
+++ R/pip_create_globals.R
@@ -76,7 +76,8 @@ pip_create_globals <- function(root_dir = Sys.getenv("PIP_ROOT_DIR"),
   glbs$CACHE_SVY_DIR_PC <- fs::path(glbs$PIP_PIPE_DIR, 'pc_data/cache/clean_survey_data/')
 
   # Old POVCalnet
-  if (Sys.getenv("R_CONFIG_ACTIVE") == "default") {
+  si <- Sys.info()
+  if (si[["user"]] == "rstudio-connect") {
     glbs$POVCALNET        <-  "//wbntpcifs/povcalnet/01.PovcalNet/"
     # Povcalnet master
     glbs$PCN_MASTER       <- fs::path(glbs$POVCALNET, "00.Master/02.vintage/")
