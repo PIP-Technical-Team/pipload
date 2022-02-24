d2f315401668c2283e96d4c8d08eb400c77bc975
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-02-24 08:52:48 -0500

    add function to convert survey_id to different variables

--- R/utils.R
+++ R/utils.R
@@ -47,3 +47,85 @@ list_of_countries <- function(root_dir          = Sys.getenv("PIP_ROOT_DIR"),
     unlist(data.table::data.table(...))
 }
 
+#' take variable `survey_id` and split it into different vars
+#'
+#' @param dt dataframe with variable `survey_id` available.
+#'
+#' @return
+#' @export
+survey_id_to_vars <- function(dt) {
+
+  # create variables for merging
+  cnames <-
+    c(
+      "country_code",
+      "surveyid_year",
+      "survey_acronym",
+      "vermast",
+      "M",
+      "veralt",
+      "A",
+      "collection",
+      "module"
+    )
+
+  fnames <-
+    c(
+      "country_code",
+      "surveyid_year",
+      "survey_acronym",
+      "vermast",
+      "veralt",
+      "collection",
+      "module",
+      "tool"
+    )
+
+  # on.exit ------------
+  on.exit({
+
+  })
+
+  # Defenses -----------
+  stopifnot(exprs = {
+    is.data.frame(dt)
+    "survey_id" %in% names(dt)
+    }
+  )
+
+  # Early returns ------
+
+  if (all(fnames %in% names(dt))) {
+    cli::cli_alert_info("variables {.var {fnames}} are already in data frame.
+                        {cli::col_blue('return the same dataframe')}")
+    return(dt)
+  }
+
+  # Computations -------
+  df <- data.table::as.data.table(dt)
+
+  df[,
+     # Name sections of surveyID into variables
+     (cnames) := tstrsplit(survey_id, "_", fixed=TRUE)
+  ][,
+
+    # create tool and source
+    tool := fifelse(module == "ALL", "TB", "PC")
+  ][,
+    # change to lower case
+    c("vermast", "veralt") := lapply(.SD, tolower),
+    .SDcols = c("vermast", "veralt")
+  ][,
+    surveyid_year := as.numeric(surveyid_year)
+  ][
+    ,
+    # Remove unnecessary variables
+    c("M", "A") := NULL
+  ]
+
+
+  # Return -------------
+  return(df)
+
+
+}
