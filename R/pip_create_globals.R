813db306897977e05fd346fa2189a0fd661611b0
Tefera19 <tdegefu@worldbank.org>
2022-02-28 16:15:57 -0500

    bpass creation of povcalnet paths

--- R/pip_create_globals.R
+++ R/pip_create_globals.R
@@ -78,9 +78,9 @@ pip_create_globals <- function(root_dir = Sys.getenv("PIP_ROOT_DIR"),
   # Old POVCalnet
   si <- Sys.info()
   if (grepl("^wb", tolower(si[["user"]]))) {
-    glbs$POVCALNET        <-  "//wbntpcifs/povcalnet/01.PovcalNet/"
+    # glbs$POVCALNET        <-  "//wbntpcifs/povcalnet/01.PovcalNet/"
     # Povcalnet master
-    glbs$PCN_MASTER       <- fs::path(glbs$POVCALNET, "00.Master/02.vintage/")
+    # glbs$PCN_MASTER       <- fs::path(glbs$POVCALNET, "00.Master/02.vintage/")
   }
 
   #
