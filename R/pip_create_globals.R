b27d0d990a5b07b625e9ae84ed395b3216048697
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-03-01 09:27:51 -0500

    solve temporary issue with povcalnet paths

--- R/pip_create_globals.R
+++ R/pip_create_globals.R
@@ -77,7 +77,9 @@ pip_create_globals <- function(root_dir = Sys.getenv("PIP_ROOT_DIR"),
 
   # Old POVCalnet
   si <- Sys.info()
-  if (grepl("^wb", tolower(si[["user"]]))) {
+  authorized <- c("wb384996", "wb499754", "wb561460") # temporal solution
+  # if (grepl("^wb", tolower(si[["user"]]))) {
+  if (tolower(si[["user"]]) %in% authorized) {
     glbs$POVCALNET        <-  "//wbntpcifs/povcalnet/01.PovcalNet/"
     # Povcalnet master
     glbs$PCN_MASTER       <- fs::path(glbs$POVCALNET, "00.Master/02.vintage/")
