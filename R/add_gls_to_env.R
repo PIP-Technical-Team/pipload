8c7881b6295edbed865e6de025621c57bbfeb757
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-06-12 11:39:39 -0400

    move add_gls_to_env to pipfun

--- R/add_gls_to_env.R
+++ R/add_gls_to_env.R
@@ -1,6 +1,8 @@
 #' Add gls list to the global envirnment. To be used in zzz.R in other packages
 #'
 #' `r lifecycle::badge("deprecated")`
+#' `add_gls_to_env` has been moved to `pipfun::add_gls_to_env()`
+#'
 #' if you don't the official value in `Sys.getenv("PIP_ROOT_DIR")` you can
 #' provide the object `root_dir  <- "<you directory>"` before executing the first
 #' function. In this way, object `gls`, which is a promise, will be
@@ -9,7 +11,7 @@
 #' @inheritParams pip_create_globals
 #'
 #' @return TRUE
-#' @export
+#' @keywords internal
 add_gls_to_env <- function(root_dir = NULL,
                            out_dir  = NULL,
                            vintage  = "latest",
@@ -17,75 +19,13 @@ add_gls_to_env <- function(root_dir = NULL,
 
   lifecycle::deprecate_soft("0.1.23",
                             "add_gls_to_env()",
-                            "pipfun::pip_create_globals()")
+                            "pipfun::add_gls_to_env()")
 
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # evaluate global environment   ---------
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-  ## defined values --------
-  obj <-  ls(pos = ".GlobalEnv")
-
-  # remove gls if it  exists
-  rm(list = obj[obj %in% c("gls")], pos = ".GlobalEnv")
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # define root_dir   ---------
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  ## if root_dir should come from Renviron --------
-
-  if (is.null(root_dir)) {
-
-    # If root_dir does not exist, create it
-    if (!("root_dir" %in% obj)) {
-      root_dir  <-  Sys.getenv("PIP_ROOT_DIR")
-      # root_dir  <-  Sys.getenv("PIP_ROOT_DIRfff")
-
-      # assign('root_dir', root_dir, envir = globalenv())
-
-    } else {
-      cli::cli_alert_info("object {.envvar root_dir} is already defined in
-                   Global env to  {.url {get('root_dir', envir = globalenv())}}.
-                   To get back to default
-                   values, make sure you remove it from memory by typing
-                   {.code rm(root_dir)}",
-                   wrap = TRUE)
-      root_dir <- get('root_dir', envir = globalenv())
-    }
-  }
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  ## If root_dir is provided by user or not found in Renviron --------
-
-  if (is.null(out_dir)) {
-    out_dir <- root_dir
-  }
-
-  # create promises and assign to global env
-  if (root_dir != "") {
-    # globals
-    gls <- pipfun::pip_create_globals(root_dir = root_dir,
-                              out_dir  = out_dir,
-                              vintage  = vintage,
-                              clean    = clean)
-    assign('gls', gls, envir = globalenv())
-
-  } else {
-
-    # assign('root_dir', "", envir = globalenv())
-    delayedAssign(x          =  "gls",
-                  value      = pipfun::pip_create_globals(root_dir = root_dir,
-                                                  out_dir  = out_dir,
-                                                  vintage  = vintage,
-                                                  clean    = clean),
-                  assign.env =  globalenv(),
-                  eval.env   = globalenv())
-  }
-
-  return(invisible(TRUE))
+  # redirecting to pipfun::add_gls_to_env
+  pipfun::add_gls_to_env(root_dir = root_dir,
+                         out_dir  = out_dir,
+                         vintage  = vintage,
+                         clean    = clean)
 
 }
 
