10cdea824c41576334b85c22ac595859d598cb4c
zander-prinsloo <prinsloo.zander@gmail.com>
2024-08-26 15:14:04 -0400

    add aux data attributes

--- R/pip_estimate_lineups.R
+++ R/pip_estimate_lineups.R
@@ -284,6 +284,176 @@ get_dist_stats <- function(df) {
 
 
 
+#' Add auxiliary data as attribute to estimated lineup data
+#'
+#' @param df data frame of estimated lineups - output of [get_refy_distributions]
+#' @inheritParams aux_data
+#'
+#' @return data frame with added attributes
+#' @export
+add_aux_data_attr <- function(df, dl_aux, df_refy, filter_aux_data = FALSE) {
+
+  code <- attr(x = df,
+               which = "country_code")
+  year <- attr(x = df,
+               which = "reporting_year")
+  reporting_level <- attr(x = df,
+                          which = "reporting_level_rows")$reporting_level
+
+  aux_data_list <- aux_data(cde             = code,
+                            yr              = year,
+                            reporting_level = reporting_level,
+                            dl_aux          = dl_aux,
+                            all             = TRUE,
+                            filter_aux_data = filter_aux_data,
+                            py              = 2017)
+
+  aux_data_list <-
+    c(aux_data_list,
+      aux_data(cde             = code,
+               yr              = year,
+               reporting_level = reporting_level,
+               dl_aux          = dl_aux,
+               all             = FALSE,
+               filter_aux_data = filter_aux_data,
+               py              = 2011))
+
+  attr(df,
+       "aux_data") <- aux_data_list
+
+  df
+}
+
+
+
+
+#' Prepare auxiliary data for attributes to be extracted for country-reference-year
+#'
+#' @param cde character: country code
+#' @param yr numeric: reference year
+#' @param reporting_level character: national, rural, urban
+#' @param dl_aux list: auxiliary data
+#' @param df_refy data frame: reference year data
+#' @param all logical: whether all aux data should be included. Default is TRUE. If FALSE, then GDP, PCE, and POP are excluded
+#' @param filter_aux_data logical: should the aux data list be filtered or not? Default is FALSE, which is means data has been pre-filtered to make appropriate selections.
+#' @param py numeric: PPP year. Default is 2017
+#'
+#' @return list
+#' @export
+aux_data <- function(cde,
+                     yr,
+                     reporting_level,
+                     dl_aux,
+                     df_refy,
+                     all = TRUE,
+                     filter_aux_data = FALSE,
+                     py = 2017) {
+
+  stopifnot(py %in% c(2011, 2017))
+  output <- list()
+
+  if (filter_aux_data) {
+    vars     <- c("ppp_year", "release_version", "adaptation_version")
+    ppp_v    <- unique(dl_aux$ppp[, ..vars], by = vars)
+    data.table::setnames(x = ppp_v,
+                         old = c("release_version", "adaptation_version"),
+                         new = c("ppp_rv", "ppp_av"))
+
+    # max release version
+    m_rv <- ppp_v[ppp_year == py, max(ppp_rv)]
+
+    # max adaptation year
+    m_av <- ppp_v[ppp_year == py & ppp_rv == m_rv,
+                  max(ppp_av)]
+
+
+    ppp <- dl_aux$ppp[ppp_year == py
+                      & release_version    == m_rv
+                      & adaptation_version == m_av
+    ][,
+      ppp_default := TRUE] |>
+      copy()
+  } else {
+    ppp <- dl_aux$ppp
+  }
+
+  # CPI
+  reporting_level <- as.character(reporting_level)
+  cpi <-
+    sapply(reporting_level,
+           \(x){
+             p <- dl_aux$cpi |>
+               fsubset(country_code == cde &
+                         cpi_year   == yr &
+                         cpi_data_level %in% c(x),
+                       get(paste0("cpi", py))) |>
+               as.numeric()
+           },
+           simplify  = FALSE,
+           USE.NAMES = TRUE)
+
+  output[[paste0("cpi", py)]] <- cpi
+
+  # PPP
+  output[[paste0("ppp", py)]] <-
+    sapply(reporting_level,
+           \(x){
+             p <- ppp |>
+               fsubset(country_code == cde &
+                         ppp_data_level %in% c(x),
+                       ppp) |>
+               as.numeric()
+           },
+           simplify  = FALSE,
+           USE.NAMES = TRUE)
+
+  # GDP
+  if (isTRUE(all)) {
+    output[["gdp"]] <-
+      dl_aux$gdp |>
+      fsubset(country_code     == cde &
+                year           == yr &
+                gdp_data_level %in% reporting_level,
+              gdp) |>
+      funique() |>
+      as.numeric()
+
+
+    # GDM - group data
+
+    # CP
+
+    # NPL
+
+    # PCE
+    output[["pce"]] <-
+      dl_aux$pce |>
+      fsubset(country_code == cde &
+                year       == yr,
+              pce) |>
+      funique() |>
+      as.numeric()
+
+    # POP
+    output[["pop"]] <-
+      sapply(reporting_level,
+             \(x){
+               p <- dl_aux$pop  |>
+                 fsubset(country_code == cde &
+                           year       == yr &
+                           pop_data_level %in% c(x),
+                         pop) |>
+                 as.numeric()
+               p
+             },
+             simplify  = FALSE,
+             USE.NAMES = TRUE)
+  }
+
+  # return
+  output
+
+}
 
 
 
